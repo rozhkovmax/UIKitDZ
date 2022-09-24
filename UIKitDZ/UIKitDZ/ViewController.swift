8498e8a9f70eee502e9c7270acddf16d4db64f6b
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-09-24 18:46:42 +0300

    1 коммит ДЗ 5

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 /// Начальный VC
 class ViewController: UIViewController {
 
+    let loginTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginLable = UILabel()
+    let passwordLable = UILabel()
+    let eyeButton = UIButton()
+    let singInButton = UIButton()
+    let imageSingIn = UIImage()
+    var imageViewSingIn = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
