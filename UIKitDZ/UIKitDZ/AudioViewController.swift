ebdb747f2423bc4880bd21ede14c18866aa1cd5f
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-09-25 10:06:47 +0300

    Сделал ДЗ Урок 6

---
+++ UIKitDZ/UIKitDZ/AudioViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AudioViewController.swift
+//  UIKitDZ
+//
+//  Created by Maxim on 24.09.2022.
+//
+
+import UIKit
+/// Импорт библиотеки плеера
+import AVFoundation
+/// Класс Аудиоплеера
+class AudioViewController: ViewController {
+
+    @IBOutlet weak var songImage: UIImageView!
+    @IBOutlet weak var songLable: UILabel!
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    
+    var image = UIImage(systemName: "q")
+    var nameSong = String()
+    var player = AVAudioPlayer()
+    var shouldPlay = false
+// Подгружаем картинку и название песни, меняем значение slider по текущему значению песни
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        songImage.image = image
+        songLable.text = nameSong
+        Timer.scheduledTimer(timeInterval: 0.1, target: self,
+                             selector: #selector(updateSlider),
+                             userInfo: nil, repeats: true)
+    }
+// Кнопка для возвращения на 1 View
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+// Slider значения коротого равняются длительности песни
+    @IBAction func sliderAudio(_ sender: Any) {
+        self.player.currentTime = TimeInterval(self.slider.value)
+    }
+// Кнопка воспроизведения и паузы
+    @IBAction func playTouchButton(_ sender: Any) {
+        
+        shouldPlay = !shouldPlay
+        if shouldPlay {
+            playMusic()
+        } else {
+            pauseMusic()
+        }
+        
+    }
+// Метод для воспроизведения и замены картинки
+    func playMusic() {
+        let pauseImage = UIImage(systemName: "pause.fill")
+        playButton.setImage(pauseImage, for: .normal)
+        self.player.play()
+    }
+// Метод для паузы и замены картинки
+    func pauseMusic() {
+        let playImage = UIImage(systemName: "play.fill")
+        playButton.setImage(playImage, for: .normal)
+        self.player.pause()
+    }
+// Метод для изменения значений slider
+    @objc func updateSlider() {
+        self.slider.value = Float(player.currentTime)
+        self.slider.maximumValue = Float(player.duration)
+    }
+    
+}
--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -6,10 +6,53 @@
 //
 
 import UIKit
+/// Импорт библиотеки плеера
+import AVFoundation
 /// Начальный VC
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var songOneImage: UIImageView!
+    @IBOutlet weak var songOneLable: UILabel!
+    
+    @IBOutlet weak var songTwoImage: UIImageView!
+    @IBOutlet weak var songTwoLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+// Кнопка перехода на 2 View с передачей картинки, названия и аудиодорожки для 1 песни
+    @IBAction func songOneButton(_ sender: Any) {
+        let storybord = UIStoryboard(name: "Main", bundle: nil)
+                     guard let audioView = storybord.instantiateViewController(withIdentifier: "AudioView")
+                             as? AudioViewController else { return }
+        audioView.image = songOneImage.image
+        audioView.nameSong = songOneLable.text ?? ""
+        music(nameMusic: "ottenki", player: &audioView.player)
+        self.present(audioView, animated: true)
+    }
+// Кнопка перехода на 2 View с передачей картинки, названия и аудиодорожки для 2 песни
+    @IBAction func songTwoButton(_ sender: Any) {
+        let storybord = UIStoryboard(name: "Main", bundle: nil)
+                     guard let audioView = storybord.instantiateViewController(withIdentifier: "AudioView")
+                             as? AudioViewController else { return }
+        audioView.image = songTwoImage.image
+        audioView.nameSong = songTwoLable.text ?? ""
+        music(nameMusic: "privychka", player: &audioView.player)
+        self.present(audioView, animated: true)
+    }
+// Метод для выбора песни
+    func music (nameMusic: String, player: inout AVAudioPlayer) {
+        do {
+            if let audio = Bundle.main.path(forResource: nameMusic, ofType: "mp3") {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audio))
+            }
+        } catch {
+            let alertController = UIAlertController(title: "Внимание!",
+                                                    message: "Аудиодорожка не найдена",
+                                                    preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default)
+            alertController.addAction(action)
+            present(alertController, animated: true, completion: nil)
+        }
     }
 }
