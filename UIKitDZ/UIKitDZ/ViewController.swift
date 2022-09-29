3f20c2f122a532a06883d0ce3d592ede1d996d40
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-09-29 18:08:52 +0300

    Исправил замечания

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 /// ShareViewController
-class ShareViewController: UIViewController, UITextFieldDelegate {
+class ShareViewController: UIViewController {
 
+    @IBOutlet weak var sharePicker: UIPickerView!
+    
     var shareButton = UIButton()
     var shareTextField = UITextField()
     var sharePickerValue = ["1 раздел", "2 раздел", "3 раздел"]
     
-    @IBOutlet weak var sharePicker: UIPickerView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        sharePicker.dataSource = self
-        sharePicker.delegate = self
-        createTextField()
-        createButton()
+        allMetod()
     }
     
-// Action с запуском Activity для кнопки
     @IBAction func activityVCActionButton(_ sender: Any) {
         let url = URL(string: "https://github.com")
         let buttonActivityViewController = UIActivityViewController(activityItems: [url ?? ""],
@@ -31,7 +27,6 @@ class ShareViewController: UIViewController, UITextFieldDelegate {
         present(buttonActivityViewController, animated: true)
     }
     
-// Создаем TextField
     func createTextField() {
         shareTextField.frame = CGRect(x: 0, y: 0, width: 300, height: 34)
         shareTextField.center = view.center
@@ -41,17 +36,22 @@ class ShareViewController: UIViewController, UITextFieldDelegate {
         view.addSubview(shareTextField)
     }
     
-// Создаем Button
     func createButton() {
         shareButton = UIButton(type: .roundedRect)
         shareButton.frame = CGRect(x: 67, y: 330, width: 280, height: 45)
         shareButton.setTitle("Расшарить", for: .normal)
-        shareButton.addTarget(self, action: #selector(hendleShare), for: .touchUpInside)
+        shareButton.addTarget(self, action: #selector(shareAction), for: .touchUpInside)
         view.addSubview(shareButton)
     }
     
-// Метод для Button, вызывающий activity
-    @objc func hendleShare(paramSender: Any) {
+    func allMetod() {
+        sharePicker.dataSource = self
+        sharePicker.delegate = self
+        createTextField()
+        createButton()
+    }
+    
+    @objc func shareAction(paramSender: Any) {
         let text = shareTextField.text
         if text?.count == 0 {
             let massege = "Cначало введите текст, потом нажмите кнопку"
@@ -68,32 +68,40 @@ class ShareViewController: UIViewController, UITextFieldDelegate {
                                                                  link ?? "nil"], applicationActivities: nil)
         present(activityViewController, animated: true, completion: nil)
     }
-    
-// Делегат TextField
+}
+
+/// UITextFieldDelegate
+extension ShareViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         shareTextField.resignFirstResponder()
         return true
     }
 }
 
-//  Расширение для Picker
+/// UIPickerViewDelegate, UIPickerViewDataSource
 extension ShareViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int { return 1 }
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return sharePickerValue.count }
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return sharePickerValue[row]
     }
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if row == 0 {
+        switch row {
+        case 0:
             view.backgroundColor = .systemGray
-        } else if row == 1 {
+        case 1:
             view.backgroundColor = .systemGray2
-        } else {
+        case 2:
             let activityPicker = UIActivityViewController(
                             activityItems: ["3 раздел Picker"],
                             applicationActivities: nil)
             present(activityPicker, animated: true, completion: nil)
+        default:
+            return
         }
     }
 }
